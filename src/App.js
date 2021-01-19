18c6077db10dd1a08fd39576d65c8ee4daa0a35b
James Hernandez <james@j-hernandez.com>
2021-01-19 18:59:17 -0500

    super useful application

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 import Home from './components/Home';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Contact />
+      <Blog />
+      <Dashboard />
     </div>
   );
 }
