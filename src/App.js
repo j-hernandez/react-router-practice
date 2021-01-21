de8e0bdb6b8a4b76cbbd188b9cb19de9c551dc1a
James Hernandez <james@j-hernandez.com>
2021-01-21 16:47:04 -0500

    adds protected route to app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Contact from './components/Contact';
 import Blog from './components/Blog';
 import Dashboard from './components/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute';
 
 import { 
   BrowserRouter as Router,
@@ -14,15 +15,25 @@ import {
   Redirect
 } from 'react-router-dom';
 
-function App() {
-  return (
+import React, { useState } from 'react';
 
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const _toggleLoggedIn = () => setIsLoggedIn(!isLoggedIn);
+
+  return (
     <Router>
       <div>
         <nav>
+          <button onClick={_toggleLoggedIn}>
+            { isLoggedIn ? 'logout' : 'login'}
+          </button>
           <Link to="/">Home Page</Link>
           <Link to="/blog">My blog</Link>
           <Link to="/contact"> Contact Me </Link>
+          { isLoggedIn && <Link to="/dashboard">Dashboard</Link>}
         </nav>
       </div>
       <Switch>
@@ -36,10 +47,12 @@ function App() {
             <Blog />
           </Route>
         <Route path="/dashboard">
-          <Dashboard />
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Dashboard />
+          </ProtectedRoute>
         </Route>
         <Route path="*">
-          <Redirect to="/dashboard" />
+          <Redirect to="/404" />
         </Route>
       </Switch>
     </Router>
